7b8d9928391fa4aa62decc0c5b398ff93d9cac2f
yerke <yerkebulan.zhaksylykov@gmail.com>
2025-09-30 10:21:45 +0500

    add db docker and register all model

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from polls.models import Question, FlightLeg, Flight
+from polls.models import (
+    Question,
+    FlightLeg,
+    Flight,
+    Choice,
+    QuestionData,
+    Publication,
+    Article,
+)
 
 
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Flight)
 admin.site.register(FlightLeg)
+admin.site.register(Choice)
+admin.site.register(QuestionData)
+admin.site.register(Publication)
+admin.site.register(Article)
