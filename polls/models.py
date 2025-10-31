7174356314b3b6680a9e0bdcc766bae39bcf4bd2
Yerkebulan Zhaksylykov <45684172+Qazaq159@users.noreply.github.com>
2025-10-31 10:33:11 +0500

    Fix1 (#1)
    * test commit: #1
    
    * test commit: #2
    
    * test commit: #3
    
    * test commit: #4
    
    ---------
    
    Co-authored-by: Yerkebulan <yerkebulan.zhaksylykov@gmail.com>

--- polls/models.py
+++ polls/models.py
@@ -122,11 +122,11 @@ class Flight(models.Model):
 class PublicationManager(models.Manager):
 
     def create(self, **kwargs):
-        constraint_fields = [field for group in self.model._meta.unique_together for field in group]
-        for field in constraint_fields:
-            if kwargs.get(field, None) is None:
-                default_val = self.model._meta.get_field(field).default
-                kwargs[field] = default_val
+        # constraint_fields = [field for group in self.model._meta.unique_together for field in group]
+        # for field in constraint_fields:
+        #     if kwargs.get(field, None) is None:
+        #         default_val = self.model._meta.get_field(field).default
+        #         kwargs[field] = default_val
 
         return super().create(**kwargs)
 
@@ -135,10 +135,10 @@ class Publication(models.Model):
     title = TranslatedField(models.CharField(max_length=30))
     create_date = models.DateTimeField(null=True, blank=True)
 
-    # objects = PublicationManager()
+    objects = PublicationManager()
 
     def __str__(self):
-        return self.title_en
+        return f'{self.title_en} - {self.create_date}'
 
     class Meta:
         # ordering = ('title_en', )
